Bugfix xyzReader, B field, etc.; added E field class and calculation routines.

bin/J_CalculateElectricField.py
@@ -16,26 +16,26 @@ def main():
         sys.exit(1)
 
     parser=argparse.ArgumentParser(
-        description="Calculate the induced magnetic field from an electron and nuclear current.",
+        description="Calculate the electric field from an electron and nuclear charge density.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
         )
 
     parser.add_argument(
         "fn",
-        nargs=3,
-        help="Charge current vectorfield as cube files (fn_x, fn_y, fn_z)."
+        help="Charge density as cube file.",
+        default=None
         )
 
     parser.add_argument(
         '--geofile',
         default='GEOMETRY',
-        help="Name of GEOMETRY file containing nuclear positions and velocities (in CPMD format)."
+        help="Name of GEOMETRY file containing nuclear positions (in CPMD format)."
         )
 
     parser.add_argument(
         '--mode',
         default='grid',
-        help="Calculate B at atom/grid positions."
+        help="Calculate E at atom/grid positions."
         )
 
     parser.add_argument(
@@ -52,54 +52,48 @@ def main():
         help="List of reference points (optional; overrides -mode)."
         )
 
-    parser.add_argument(
-        '-v',
-        type=float,
-        nargs=3,
-        default=[0.0, 0.0, 0.0],
-        help="The velocity of the reference frame in a.u. Non-zero value requires the charge \
-        density unless set argument --nuclei_only. "
-        )
-
-    parser.add_argument(
-        "--density",
-        help="Charge density as cube file (only for moving reference; see -v option).",
-        default=None
-        )
+#    parser.add_argument(
+#        '-v',
+#        type=float,
+#        nargs=3,
+#        default=[0.0, 0.0, 0.0],
+#        help="The velocity of the reference frame in a.u. Non-zero value requires the charge \
+#        density unless set argument --nuclei_only. "
+#        )
 
     parser.add_argument(
        '--use_valence_charges',
        action='store_true',
        default=False,
-       help="Calculate the nuclear contribution to the B field using atomic valence charges only (e.g., when using non-local pseudopotentials)."
+       help="Calculate the nuclear contribution to the E field using atomic valence charges only (e.g., when using non-local pseudopotentials)."
        )
 
     parser.add_argument(
-       '--j_sparse',
+       '--q_sparse',
        type=int,
        default=1,
-       help="Sparsity of j field."
+       help="Sparsity of charge density."
        )
 
     parser.add_argument(
        '--r_sparse',
        type=int,
        default=1,
-       help="Sparsity of position grid (grid mode only; multiplies with j_sparse)."
+       help="Sparsity of position grid (grid mode only; multiplies with q_sparse)."
        )
 
     parser.add_argument(
-       '--j_crop',
+       '--q_crop',
        type=int,
        default=None,
-       help="Crop outer n points of j field."
+       help="Crop outer n points of charge density."
        )
 
     parser.add_argument(
        '--r_crop',
        type=int,
        default=None,
-       help="Crop outer n points of position grid (grid mode only; adds to j_crop)."
+       help="Crop outer n points of position grid (grid mode only; adds to q_crop)."
        )
 
     parser.add_argument(
@@ -113,14 +107,14 @@ def main():
        '--nuclei_only',
        action="store_true",
        default=False,
-       help="Calculate only the nuclear contribution to the B field."
+       help="Calculate only the nuclear contribution to the E field."
        )
 
     parser.add_argument(
        '--electrons_only',
        action="store_true",
        default=False,
-       help="Calculate only the electronic contribution to the B field."
+       help="Calculate only the electronic contribution to the E field."
        )
 
     parser.add_argument(
@@ -140,54 +134,41 @@ def main():
 
     if args.kspace:
         args.mode = "grid"
-        print( 'WARNING: k space calculation still in alpha state (and no nuclear B field component, either)!' )
+        raise NotImplementedError('Please use the real space calculation!')
 
     for _k in _attr:
         if getattr(args,_k) not in _attr[_k]: raise AttributeError('Wrong argument for %s!' % _k)
-
-    ref_vel_au = np.array(args.v)
-    if np.linalg.norm(ref_vel_au) != 0.0 and not args.nuclei_only:
-        if args.density is None:
-            raise AttributeError('Please set the --density argument for moving reference frame!')
 
     _S = list()
     _S.append(time.time())
 
     #-------- Read input and crop --------------
-    j = volume.VectorField(*args.fn)
+    rho = volume.ScalarField(args.fn)
 
-    if np.linalg.norm(ref_vel_au) != 0.0 and not args.nuclei_only:
-        rho = volume.ScalarField(args.density)
-        rho._is_similar(j, strict=2)
-        j.data -= ref_vel_au[:, None, None, None] * rho.data[None]
-        del rho
-
-    j.helmholtz_decomposition()
-    j.data = j.solenoidal_field
-    if args.j_crop is not None:
-        j.crop(args.j_crop)
-    j.sparsity(args.j_sparse)
-    j.print_info()
+    if args.q_crop is not None:
+        rho.crop(args.q_crop)
+    rho.sparsity(args.q_sparse)
+    rho.print_info()
 
     #----------- Get vel and Q for atoms -------- 
     if not args.electrons_only:
         try:
-            nuc = trajectory.XYZFrame(args.geofile, numbers=j.numbers, fmt='cpmd',
+            nuc = trajectory.XYZFrame(args.geofile, numbers=rho.numbers, fmt='cpmd',
                     filetype='GEOMETRY')
-            if not np.allclose(j.pos_au, nuc.pos_aa*constants.l_aa2au):
+            if not np.allclose(rho.pos_au, nuc.pos_aa*constants.l_aa2au):
                 raise ValueError("The given cube files do not correspond to %s!" % args.geofile)
             #Compatibility warning: In CPMD xyz velocities are given in aa per t_au! Multiplication by l_aa2au compulsory!
             #Or use CPMD format
-            vel_au = nuc.vel_au - ref_vel_au[None]#*constants.l_aa2au
+            pos_au = rho.pos_au
             if args.use_valence_charges:
                 _key = "ZV"
             else:
                 _key = "Z"
             Q = [constants.species[_s][_key] for _s in nuc.symbols]
-            print('Nuclear Velocities/Charges')
+            print('Nuclear Positions/Charges')
             print(77 * '–')
             print( '%4s '%'#' + ' '.join( "%12s" % _s for _s in ['v_x', 'v_y', 'v_z', 'Q']))
-            for _i, (_v, _q) in enumerate(zip(vel_au, Q)):
+            for _i, (_v, _q) in enumerate(zip(pos_au, Q)):
                 print( '%4d '%_i
                     + ' '.join(map('{:12.5g}'.format, _v)) + ' '
                     + '{:12.5g}'.format(_q)
@@ -203,14 +184,14 @@ def main():
     #------- Get R and crop again -------------- 
     if args.r is None:
        if args.mode == "grid":
-           _tmp = copy.deepcopy(j)
+           _tmp = copy.deepcopy(rho)
            if args.r_crop is not None:
                _tmp.crop(args.r_crop)
            _tmp.sparsity(args.r_sparse)
            R = _tmp.pos_grid()
 
        if args.mode == "atom":
-           R = j.pos_au.T
+           R = rho.pos_au.T
     #else:
     #    R = np.array(args.r).T.astype(float)
 
@@ -220,8 +201,8 @@ def main():
 
     #---------- Get B -------------------------- 
     if not args.nuclei_only:
-        B1 = field.MagneticField.from_current(
-                j,
+        E1 = field.ElectricField.from_charge_density(
+                rho,
                 R=R,
                 verbose=args.verbose,
                 nprocs=args.nprocs,
@@ -229,12 +210,11 @@ def main():
                 )
 
     if not args.electrons_only:
-        B2 = field.MagneticField.from_moving_point_charges(
-                j.pos_au,
-                vel_au,
+        E2 = field.ElectricField.from_point_charges(
+                pos_au,
                 Q,
                 R=R,
-                **extract_keys(vars(j), cell_vec_au=None, origin_au=None, numbers=None, pos_au=None)
+                **extract_keys(vars(rho), cell_vec_au=None, origin_au=None, numbers=None, pos_au=None)
                 )
 
     _S.append(time.time())
@@ -244,22 +224,22 @@ def main():
 
     if args.mode == 'grid':
         if not args.nuclei_only:
-            B1.write(
-                "B_e-1.cube", "B_e-2.cube", "B_e-3.cube",
-                comment1 = 3*["B field induced by electron current"],
+            E1.write(
+                "E_e-1.cube", "E_e-2.cube", "E_e-3.cube",
+                comment1 = 3*["E field by electron density"],
             )
 
         if not args.electrons_only:
-            B2.write(
-                "B_n-1.cube", "B_n-2.cube", "B_n-3.cube",
-                comment1 = 3*["B field induced by nuclear motion"],
+            E2.write(
+                "E_n-1.cube", "E_n-2.cube", "E_n-3.cube",
+                comment1 = 3*["E field by nuclear charges"],
             )
 
         if not any([args.nuclei_only, args.electrons_only]):
-            B1.data += B2.data
-            B1.write(
-                "B_total-1.cube", "B_total-2.cube", "B_total-3.cube",
-                comment1 = 3*["B field induced by nuclear motion and electron current"],
+            E1.data += E2.data
+            E1.write(
+                "E_total-1.cube", "E_total-2.cube", "E_total-3.cube",
+                comment1 = 3*["E field nuclear charges and electron density"],
             )
 
     elif args.r is not None or args.mode == "atom":
@@ -267,11 +247,11 @@ def main():
         print( 160 * '–' )
         print( '%4s '%'#' + ' '.join( "%12s" % _s for _s in [
             'rx', 'ry', 'rz',
-            'Bx_e', 'By_e', 'Bz_e',
-            'Bx_n', 'By_n', 'Bz_n',
-            'Bx_tot', 'By_tot', 'Bz_tot'
+            'Ex_e', 'Ey_e', 'Ez_e',
+            'Ex_n', 'Ey_n', 'Ez_n',
+            'Ex_tot', 'Ey_tot', 'Ez_tot'
         ]) )
-        for _i, (_r,_b1,_b2) in enumerate(zip(R.T, B1.data.T, B2.data.T)):
+        for _i, (_r,_b1,_b2) in enumerate(zip(R.T, E1.data.T, E2.data.T)):
             print( '%4d '%_i
                    + ' '.join( map( '{:12.5g}'.format, _r) ) + ' '
                    + ' '.join( map( '{:12.5g}'.format, _b1) ) + ' '
@@ -289,7 +269,7 @@ def main():
 
     print('\n'.join( [
         '%-20s %10.5f' % _t for _t in zip(
-            ["Reading input" ,"B field calculation" ,"Writing output" ],
+            ["Reading input" ,"E field calculation" ,"Writing output" ],
             np.diff(_S) #[ _s - _S[0] for _s in _S[1:] ]
         )
     ]))

classes/field.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 from multiprocessing import Manager, Process
 from chirpy.physics.classical_electrodynamics import biot_savart, biot_savart_grid, biot_savart_kspace
+from chirpy.physics.classical_electrodynamics import coulomb, coulomb_grid, coulomb_kspace
 #import copy
 
 from ..classes.volume import VectorField
@@ -14,10 +15,10 @@ class MagneticField(VectorField):
 
     @classmethod
     def from_current(cls, j, **kwargs):
-        '''Calculate B under the magnetostatic approximation'''
+        '''Calculate B under the magnetostatic approximation (Biot-Savart law)'''
         verbose = kwargs.get('verbose', False)
         nprocs = kwargs.get('nprocs', 1)
-        #---- default: electrons rotating (charge -1)
+        #---- default: electrons moving (charge -1)
         charge = kwargs.pop('charge', -1)
         if kwargs.pop('kspace', False):
             sys.stdout.flush()
@@ -80,9 +81,9 @@ class MagneticField(VectorField):
 
 
     @classmethod
-    def from_moving_point_charges(cls, pos_au, vel_au, Q, **kwargs):
+    def from_moving_point_charges(cls, P_au, V_au, Q, **kwargs):
         '''
-        pos_au/vel_au...positions velocities (N,3)
+        P_au/V_au...positions velocities (N,3)
         Q...charge (in e)
         R...of shape (3, n_positions)
         '''
@@ -91,26 +92,113 @@ class MagneticField(VectorField):
         if verbose:
             print('Calculating nuclear contribution to the B field...')
 
-        R = kwargs.pop('R', pos_au.T)
+        R = kwargs.pop('R', P_au.T)
         _shape = R.shape
         _npoints = np.prod(R.shape[1:])
 
-        #if args.mode == "grid":
-        #      #we do not need old R anymore
         #flattening
         R = np.array([cls._read_vec(R, _ip) for _ip in range(_npoints)]).T
         _tmp_B = np.zeros_like(R.T)
 
-        for _p, _v, _q in zip(pos_au, vel_au, Q):
+        for _p, _v, _q in zip(P_au, V_au, Q):
             #change thresh because closest points will explode expression
             _tmp_B += biot_savart(R.T, _p[None,:], _v[None,:]*_q, thresh=0.01) #j.voxel**(1/3))
         B2 = _tmp_B.T.reshape(_shape) * charge
-        del _tmp_B
         if verbose:
             print( "Done." )
         return cls.from_data(data=B2, **kwargs)
 
 class ElectricField(VectorField):
     @classmethod
-    def from_charge_density(cls):
-        pass
+    def from_charge_density(cls, rho, **kwargs):
+        '''Calculate E under the electrostatic approximation (Coulomb's law)'''
+        verbose = kwargs.get('verbose', False)
+        nprocs = kwargs.get('nprocs', 1)
+        #---- default: electrons moving (charge -1)
+        charge = kwargs.pop('charge', -1)
+        if kwargs.pop('kspace', False):
+            sys.stdout.flush()
+            E1 = coulomb_kspace(rho.data, rho.cell_au, rho.voxel)
+
+        else:
+            #------------------------------------------- 
+            #     Multiprocessing
+            #------------------------------------------- 
+
+            #------- Some definitions ------------------ 
+            def _parallel_f(R, E, n):
+                E[n] = coulomb_grid(R, rho.data, rho.pos_grid(), rho.voxel)
+
+            def _collect():
+                for _ip in range(_npoints):
+                    cls._write_vec(E1, _ip, result_dict[_ip])
+                    del result_dict[_ip]
+
+
+            #------- Get R -----------------------------
+            R = kwargs.pop('R', rho.pos_grid())
+            _npoints = np.prod(R.shape[1:])
+            if verbose:
+                print("No. of grid points: %d" % _npoints)
+            if verbose:
+                print('Calculating E field on %d core(s)...' % nprocs )  #Could be improved in performance. Normalisation?
+
+            #------- Init --------------------------- 
+            E1 = np.zeros_like(R)
+            #------- Start parallel process --------- 
+            _n = 0
+            manager = Manager()
+            result_dict = manager.dict()
+            jobs = list()
+
+            for _ip in range(_npoints):
+                if _ip%100 == 0 and verbose:
+                    print(_ip, end="\r")
+                    sys.stdout.flush()
+                _p = Process(target=_parallel_f, args=(cls._read_vec(R, _ip), result_dict, _ip))
+                jobs.append(_p)
+                _p.start()
+                _n += 1
+                if _n == nprocs:
+                    for proc in jobs:
+                        proc.join()
+                    _n = 0
+                    jobs = list()
+            for proc in jobs:
+                 proc.join()
+
+            _collect()
+            E1 *= charge
+            if verbose:
+                print( "Done.           " )
+        nargs = vars(rho)
+        nargs.update({'data':E1})
+        return cls.from_data(**nargs)
+
+    @classmethod
+    def from_point_charges(cls, P_au, Q, **kwargs):
+        '''
+        P_au...positions velocities (N,3)
+        Q...charge (in e)
+        R...of shape (3, n_positions)
+        '''
+        charge = kwargs.pop('charge', +1)
+        verbose = kwargs.get('verbose', False)
+        if verbose:
+            print('Calculating nuclear contribution to the E field...')
+
+        R = kwargs.pop('R', P_au.T)
+        _shape = R.shape
+        _npoints = np.prod(R.shape[1:])
+
+        #flattening
+        R = np.array([cls._read_vec(R, _ip) for _ip in range(_npoints)]).T
+        _tmp_E = np.zeros_like(R.T)
+
+        for _p, _q in zip(P_au, Q):
+            #change thresh because closest points will explode expression
+            _tmp_E += coulomb(R.T, _p[None,:], _q, thresh=0.01)
+        E2 = _tmp_E.T.reshape(_shape) * charge
+        if verbose:
+            print( "Done." )
+        return cls.from_data(data=E2, **kwargs)

classes/volume.py
@@ -99,7 +99,11 @@ class ScalarField():
         if any([cell_vec_au.size == 0, data.size == 0]):
             raise AttributeError('ERROR: Please give both, cell_vec_au and data!')
         obj = cls()
-        obj.comments = 'no_comment\nno_comment'
+        #quick workaround to find out if vectorfield
+        if data.shape[0]==3:
+            obj.comments = 3*['no_comment\nno_comment']
+        else:
+            obj.comments = 'no_comment\nno_comment'
         obj.origin_au = kwargs.get('origin_au', np.zeros((3)))
         obj.pos_au = kwargs.get('pos_au', np.zeros((0, 3)))
         obj.numbers = kwargs.get('numbers', np.zeros((0, )))
@@ -401,8 +405,8 @@ class VectorField(ScalarField):
         fmt  = kwargs.get('fmt',fn1.split('.')[-1])
         attr = kwargs.get('attribute','data')
         if fmt=="cube":
-            comment1 = kwargs.get('comment1',self.comments.split('\n')[0])
-            comment2 = kwargs.get('comment2',self.comments.split('\n')[1])
+            comment1 = kwargs.get('comment1',[c.split('\n')[0] for c in self.comments])
+            comment2 = kwargs.get('comment2',[c.split('\n')[1] for c in self.comments])
             pos_au   = kwargs.get('pos_au',self.pos_au)
             numbers  = kwargs.get('numbers',self.numbers)
             cell_au  = kwargs.get('cell_au',self.cell_au)

generators/gen_box.py
@@ -70,7 +70,10 @@ class _BoxObject():
             nargs[ 'cell_aa_deg' ] = np.array( get_atom_spread( _sys.XYZData.data ) )
 
         #pass pbc? cell? WARNING: do not just pass kwargs! reassemble it!!
-        return cls( members = [ ( 1, _s._to_frame() ) for _s in _sys.XYZData._split( _sys.mol_map ) ], **nargs )
+        if _sys.mol_map is not None:
+            return cls(members=[(1, _s._to_frame()) for _s in _sys.XYZData._split(_sys.mol_map)], **nargs)
+        else:
+            return cls(members=[(1, _sys.XYZData._to_frame())], **nargs)
 
     def _cell_vec_aa( self, **kwargs ):
         return get_cell_vec( kwargs.get( 'cell_aa_deg' ) )
@@ -94,7 +97,7 @@ class _BoxObject():
         except:
             pass
     #def routine: check all xx attributes against _xx() methods
-        
+
     def _clean_members( self ):
         if self.n_members == 0: return None
         _eq = np.zeros( ( self.n_members, ) * 2 )

physics/classical_electrodynamics.py
@@ -22,7 +22,7 @@ def current_dipole_moment(vel_au,charges):
 
 #def magnetic_dipole_shift_origin(pos_au,c_au,origin_au=np.array([0.0,0.0,0.0]),cell_au=None):
 def magnetic_dipole_shift_origin_OLD(pos_au,c_au,origin_au,**kwargs):
-    cell_au = kwargs.get('cell_au')   
+    cell_au = kwargs.get('cell_au')
     v_trans_au = pos_au-origin_au[None,:]
     if hasattr(cell_au,'shape'): #check for np array, not so pretty
         v_trans_au -= np.around(v_trans_au/cell_au)*cell_au
@@ -35,18 +35,40 @@ def magnetic_dipole_shift_origin(c_au,trans_au,**kwargs):
         return 0.5*np.sum(eijk[None,None,:,:,:]*trans_au[:,:,:,None,None]*c_au[:,:,None,:,None], axis=(2,3))#sum over mols (axis 1) done later
 
 #def magnetic_dipole_moment_shift_origin_pbc(m_old_au,pos_au,origin_au=np.array([0.0,0.0,0.0])):
+def coulomb(r0, r, q, thresh=1.E-8):
+    '''r...shape(N,...,3)'''
+    d = r0 - r
+    d3 = np.linalg.norm(d, axis=-1)**3
+    with np.errstate(divide='ignore'):
+        d3_inv = np.where(d3<thresh, 0.0, np.divide(1.0, d3))
+    E = q * d * d3_inv[:,None]
+    return E
+
+def coulomb_grid(r, rho, pos_grid, voxel, thresh=1.E-8):
+    '''r...shape(3,...,N)'''
+    d = r[:, None, None, None]-pos_grid
+    d3 = np.linalg.norm(d, axis=0)**3
+    with np.errstate(divide='ignore'):
+        d3_inv = np.where(d3<thresh, 0.0, np.divide(1.0, d3))
+    E = rho[None] * d * d3_inv[None] * voxel
+    E = E.sum(axis=(1, 2, 3))
+    return E
+
+def coulomb_kspace(rho, cell_au, voxel):
+    pass
 
 def biot_savart(r0, r, j, thresh=1.E-8):
-    '''r...shape(N,N,...,3)'''
+    '''r...shape(N,...,3)'''
     #in atomic units using cgs convention for B field (µ0 = 1/c)
     d = r0 - r
-    d2 = np.linalg.norm(d, axis=-1)**2
+    d3 = np.linalg.norm(d, axis=-1)**3
     with np.errstate(divide='ignore'):
-        d2_inv = np.where(d2<thresh, 0.0, np.divide(1.0, d2))
-    B = np.cross(j,d,axisa=-1,axisb=-1,axisc=-1) * d2_inv[:,None]
+        d3_inv = np.where(d3<thresh, 0.0, np.divide(1.0, d3))
+    B = np.cross(j,d,axisa=-1,axisb=-1,axisc=-1) * d3_inv[:,None]
     return B / constants.c_au
 
 def biot_savart_grid(r, j, pos_grid, voxel, thresh=1.E-8):
+    '''r...shape(3,...,N)'''
     #in atomic units using cgs convention for B field (µ0 = 1/c)
     d = r[:, None, None, None]-pos_grid
     d3 = np.linalg.norm(d, axis=0)**3
@@ -64,7 +86,7 @@ def biot_savart_kspace(j, cell_au, voxel):
     B2 = kspace.k_potential(rot[1], cell_au)[1]
     B3 = kspace.k_potential(rot[2], cell_au)[1]
     B = np.array([B1, B2, B3]) / (4 * np.pi) #this 4 pi division should be done in kspace binary already?
-    
+
     #G == 0 ???
     return B * voxel / constants.c_au #*2??
 

readers/trajectory.py
@@ -62,7 +62,9 @@ def xyzReader(FN, **kwargs):
     with open(FN, 'r') as _f:
         _nlines = int(_f.readline().strip()) + 2
 
-    return zip(*_reader(FN, _nlines, _kernel, **kwargs))
+    #ToDo: add test on whether all frames have the same symbols
+    data, symbols, comments = zip(*_reader(FN, _nlines, _kernel, **kwargs))
+    return np.array(data), symbols[0], list(comments)
 
 
 def cpmdReader(FN, **kwargs):