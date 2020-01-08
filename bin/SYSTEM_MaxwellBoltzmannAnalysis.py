Added fixed degrees of freedom to calculation of temperature

bin/SYSTEM_MaxwellBoltzmannAnalysis.py
@@ -14,6 +14,7 @@
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+import warnings
 
 from chirpy.physics import statistical_mechanics
 from chirpy.classes import system
@@ -57,7 +58,7 @@ def main():
             "--T",
             help="Reference temperature in K.",
             type=float,
-            default=298.15,
+            default=None,
             )
     parser.add_argument(
             "--element",
@@ -105,8 +106,23 @@ def main():
         except StopIteration:
             pass
 
-    _vel_au = np.linalg.norm([_v[args.subset] for _v in get_v()],
-                             axis=-1).ravel()
+    _vel_au = np.array([_v[args.subset] for _v in get_v()])
+    e_kin_au = statistical_mechanics.kinetic_energies(_vel_au, _w[args.subset])
+    _n_f_dof = 6
+    if args.element is not None:
+        warnings.warn('Element mode: Switching off conservation of '
+                      'degrees of freedom.')
+        _n_f_dof = 0
+    T_K = np.average(statistical_mechanics.temperature_from_energies(
+                                            e_kin_au,
+                                            fixed_dof=_n_f_dof
+                                            ))
+    print(f'MD temperature: {T_K} K')
+    if args.T is None:
+        args.T = T_K
+
+    _vel_au = np.linalg.norm(_vel_au, axis=-1).ravel()
+
     plt.hist(_vel_au, lw=0.1, ec='k', bins=100, density=True, label=args.fn)
 
     _ideal = [statistical_mechanics.maxwell_boltzmann_distribution(
@@ -126,6 +142,8 @@ def main():
     plt.title(title)
     plt.legend(loc='upper right')
     plt.show()
+
+
 
 if __name__ == "__main__":
     main()

physics/statistical_mechanics.py
@@ -18,21 +18,26 @@ from ..physics import constants
 
 def kinetic_energies(vel_au, masses_amu):
     """Calculate kinetic energy in a.u. from given velocities in a.u.
-       and masses in a.m.u."""
-    n_frames = vel_au.shape[0]
-    n_atoms = vel_au.shape[1]
-    e_kin_au = np.zeros((n_frames, n_atoms))
-    for i_at in range(n_atoms):
-        e_kin_au[:, i_at] = masses_amu[i_at] *\
-                np.square(vel_au[:, i_at]).sum(axis=1)
+       and masses in a.m.u.
+       velocities of shape ([n_frames,] n_atoms, three)"""
+
+    e_kin_au = np.square(vel_au).sum(axis=-1) * masses_amu
     return 0.5 * e_kin_au * constants.m_amu_au
+
+
+def temperature_from_energies(e_kin_au, fixed_dof=6):
+    '''Ideal gas after Boltzmann.
+       fixed_dof=6 ... fixed total linear and angular momentum.
+       Expects kinetic energies of shape ([n_frames,] n_atoms)'''
+    _n_dof = 3 * e_kin_au.shape[-1] - fixed_dof
+    return (2. * e_kin_au / constants.k_B_au / _n_dof).sum(axis=-1)
 
 
 def maxwell_boltzmann_distribution(T_K, *args, **kwargs):
     '''Return the Maxwell-Boltzmann distribution function for given temperature
        in K and species with masses in a.m.u.'''
 
-    def _velocity_distribution(T_K, vel_au, mass_amu):
+    def _velocity_distribution(T_K, vel_norm_au, mass_amu):
         '''Returns the probability density of a given velocity in a.u. of a
            particle with mass in a.m.u. at a given temperature in K.
            Accepts np.arrays of species if shapes of vel and mass are equal.'''
@@ -40,9 +45,9 @@ def maxwell_boltzmann_distribution(T_K, *args, **kwargs):
         beta = constants.k_B_au * T_K
 
         N = np.sqrt(2 / np.pi) * pow(m_au / beta, 3.0/2.0)
-        p1 = np.exp(-(m_au * vel_au**2) / (2 * beta))
+        p1 = np.exp(-(m_au * vel_norm_au**2) / (2 * beta))
 
-        return N * p1 * vel_au**2
+        return N * p1 * vel_norm_au**2
 
     def _energy_distribution(T_K, E_au):
         '''Returns the probability density of a given energy in a.u.