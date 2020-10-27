Bugfix in CPMDWriter and Gauge creator (units, units, ...); bugfix in writer tests (circular variable pass impeded finding wrongly written cpmd files)

chirpy-test/interface.py
@@ -33,6 +33,7 @@ import os
 import numpy as np
 import warnings
 import filecmp
+import copy
 
 from chirpy.interface import cpmd
 from chirpy.physics import constants
@@ -86,25 +87,36 @@ class TestCPMD(unittest.TestCase):
             ))
 
     def test_cpmdWriter(self):
-        data = cpmd.cpmdReader(self.dir + '/TRAJECTORY',
-                               filetype='TRAJECTORY',
-                               symbols=['X']*208)['data']
+        data_r = cpmd.cpmdReader(self.dir + '/TRAJECTORY',
+                                 filetype='TRAJECTORY',
+                                 symbols=['X']*208)['data']
 
-        cpmd.cpmdWriter(self.dir + '/OUT', data, write_atoms=False)
+        _outfile = 'OUT_cpmd_w'
+        # --- important in case writer manipulates input
+        data = copy.deepcopy(data_r)
         with self.assertRaises(ValueError):
             # --- sorted data
-            cpmd.cpmdWriter(self.dir + '/OUT', data, symbols=['X', 'Y']*104,
+            cpmd.cpmdWriter(_outfile, data, symbols=['X', 'Y']*104,
                             write_atoms=True)
+        cpmd.cpmdWriter(_outfile, data,
+                        frames=np.arange(data.shape[0]).astype(int)+1,
+                        write_atoms=False)
 
         with warnings.catch_warnings():
             warnings.filterwarnings('ignore', category=UserWarning)
-            data2 = cpmd.cpmdReader(self.dir + '/OUT',
+            data2 = cpmd.cpmdReader(_outfile,
                                     filetype='TRAJECTORY',
-                                    symbols=cpmd.cpmd_kinds_from_file(self.dir
-                                                                      + '/OUT')
+                                    symbols=cpmd.cpmd_kinds_from_file(_outfile)
                                     )['data']
-        self.assertTrue(np.allclose(data, data2, atol=0.0))
-        os.remove(self.dir + "/OUT")
+        self.assertTrue(np.allclose(data_r, data2, atol=0.0))
+        self.assertTrue(filecmp.cmp(_outfile,
+                                    self.dir + '/TRAJECTORY',
+                                    shallow=False),
+                        f'CPMD file {self.dir}/TRAJECTORY reproduced '
+                        f'incorrectly in {_outfile}'
+                        )
+
+        os.remove(_outfile)
 
     def test_cpmdjob(self):
         # --- insufficiently tested

chirpy-test/write.py
@@ -32,6 +32,7 @@ import unittest
 import os
 import numpy as np
 import warnings
+import copy
 
 from chirpy.read import modes as r_modes
 from chirpy.read import coordinates as r_coordinates
@@ -82,47 +83,55 @@ class TestCoordinates(unittest.TestCase):
         pass
 
     def test_xyzWriter(self):
-        data, symbols, comments = \
-            r_coordinates.xyzReader(self.dir + '/test_frame_posvel_pbc.xyz')
+        _ref = r_coordinates.xyzReader(self.dir + '/test_frame_posvel_pbc.xyz')
+        data_r, symbols_r, comments_r = _ref
+        data, symbols, comments = copy.deepcopy(_ref)
         w_coordinates.xyzWriter(self.dir + '/out.xyz', data, symbols, comments)
         data2, symbols2, comments2 = \
             r_coordinates.xyzReader(self.dir + '/out.xyz')
 
-        self.assertTupleEqual(symbols, symbols2)
-        self.assertListEqual(comments, comments2)
-        self.assertTrue(np.array_equal(data, data2))
+        self.assertTupleEqual(symbols_r, symbols2)
+        self.assertListEqual(comments_r, comments2)
+        self.assertTrue(np.array_equal(data_r, data2))
 
-        data, symbols, comments = \
-            r_coordinates.xyzReader(self.dir + '/test_traj_pos_pbc.xyz')
+        _ref = r_coordinates.xyzReader(self.dir + '/test_traj_pos_pbc.xyz')
+        data_r, symbols_r, comments_r = _ref
+        data, symbols, comments = copy.deepcopy(_ref)
         w_coordinates.xyzWriter(self.dir + '/out.xyz', data, symbols, comments)
         data2, symbols2, comments2 = \
             r_coordinates.xyzReader(self.dir + '/out.xyz')
 
-        self.assertTupleEqual(symbols, symbols2)
-        self.assertListEqual(comments, comments2)
-        self.assertTrue(np.array_equal(data, data2))
+        self.assertTupleEqual(symbols_r, symbols2)
+        self.assertListEqual(comments_r, comments2)
+        self.assertTrue(np.array_equal(data_r, data2))
         os.remove(self.dir + "/out.xyz")
 
     def test_arcWriter(self):
+        _ref = r_coordinates.arcReader(self.dir + '/water.arc')
+        data_r, symbols_r, indices_r, types_r, connectivity_r, comments_r = \
+            _ref
         data, symbols, indices, types, connectivity, comments = \
-            r_coordinates.arcReader(self.dir + '/water.arc')
+            copy.deepcopy(_ref)
         w_coordinates.arcWriter(self.dir + '/out.arc', data, symbols, types,
                                 connectivity, comments)
         data2, symbols2, indices2, types2, connectivity2, comments2 = \
             r_coordinates.arcReader(self.dir + '/out.arc')
 
-        self.assertTupleEqual(symbols, symbols2)
-        self.assertTupleEqual(types, types2)
-        self.assertTupleEqual(connectivity, connectivity2)
-        self.assertTupleEqual(indices, indices2)
-        self.assertListEqual(comments, comments2)
-        self.assertTrue(np.array_equal(data, data2))
+        self.assertTupleEqual(symbols_r, symbols2)
+        self.assertTupleEqual(types_r, types2)
+        self.assertTupleEqual(connectivity_r, connectivity2)
+        self.assertTupleEqual(indices_r, indices2)
+        self.assertListEqual(comments_r, comments2)
+        self.assertTrue(np.array_equal(data_r, data2))
 
         os.remove(self.dir + "/out.arc")
 
     def test_pdbWriter(self):
-        data, names, symbols, res, cell_aa_deg, title =\
-            r_coordinates.pdbReader(self.dir + '/test_simple.pdb')
+        _ref = r_coordinates.pdbReader(self.dir + '/test_simple.pdb')
+        data_r, names_r, symbols_r, res_r, cell_aa_deg_r, title_r = \
+            _ref
+        data, names, symbols, res, cell_aa_deg, title = \
+            copy.deepcopy(_ref)
 
         # --- only single frames for now
         w_coordinates.pdbWriter(self.dir + '/out.pdb', data[0], names, symbols,
@@ -132,11 +141,11 @@ class TestCoordinates(unittest.TestCase):
             data2, names2, symbols2, res2, cell_aa_deg2, title2 =\
                 r_coordinates.pdbReader(self.dir + '/out.pdb')
 
-        self.assertTupleEqual(names, names2)
-        self.assertTupleEqual(symbols, symbols2)
-        self.assertTupleEqual(res, res2)
-        self.assertTrue(np.array_equal(data, data2))
-        self.assertListEqual(cell_aa_deg.tolist(), cell_aa_deg2.tolist())
+        self.assertTupleEqual(names_r, names2)
+        self.assertTupleEqual(symbols_r, symbols2)
+        self.assertTupleEqual(res_r, res2)
+        self.assertTrue(np.array_equal(data_r, data2))
+        self.assertListEqual(cell_aa_deg_r.tolist(), cell_aa_deg2.tolist())
         os.remove(self.dir + "/out.pdb")
 
 
@@ -149,8 +158,11 @@ class TestGrid(unittest.TestCase):
         pass
 
     def test_cubeWriter(self):
+        _ref = r_grid.cubeReader(self.dir + '/test-1.cube')
+        data_r, origin_aa_r, cell_vec_aa_r, pos_aa_r, numbers_r, comments_r = \
+            _ref
         data, origin_aa, cell_vec_aa, pos_aa, numbers, comments = \
-                r_grid.cubeReader(self.dir + '/test-1.cube')
+            copy.deepcopy(_ref)
         w_grid.cubeWriter(self.dir + '/out.cube',
                           comments[0],
                           numbers,
@@ -161,10 +173,10 @@ class TestGrid(unittest.TestCase):
         data2, origin_aa2, cell_vec_aa2, pos_aa2, numbers2, comments2 = \
             r_grid.cubeReader(self.dir + '/out.cube')
 
-        self.assertTrue(np.array_equal(data, data2))
-        self.assertTrue(np.array_equal(origin_aa, origin_aa2))
-        self.assertTrue(np.array_equal(cell_vec_aa, cell_vec_aa2))
-        self.assertTrue(np.array_equal(pos_aa, pos_aa2))
-        self.assertTupleEqual(numbers, numbers2)
-        self.assertListEqual(comments, comments2)
+        self.assertTrue(np.array_equal(data_r, data2))
+        self.assertTrue(np.array_equal(origin_aa_r, origin_aa2))
+        self.assertTrue(np.array_equal(cell_vec_aa_r, cell_vec_aa2))
+        self.assertTrue(np.array_equal(pos_aa_r, pos_aa2))
+        self.assertTupleEqual(numbers_r, numbers2)
+        self.assertListEqual(comments_r, comments2)
         os.remove(self.dir + "/out.cube")

chirpy/classes/object.py
@@ -75,11 +75,11 @@ class Sphere(_CORE):
            BETA
            '''
 
-        # NB: Manipulation of x is retained outside of this function!
-
         def get_d(orig, _pos):
             return np.linalg.norm(distance_pbc(orig, _pos, cell=cell), axis=-1)
 
+        # --- never manipulate input
+        _x = copy.deepcopy(x)
         keep = copy.deepcopy(x)
 
         if len(pos.shape) > 3:
@@ -96,9 +96,9 @@ class Sphere(_CORE):
                             (pos.shape, self.pos.shape))
 
         _slc = (slice(None),) * len(_d.shape) + (None,)
-        x *= self.edge(_d)[_slc]
+        _x *= self.edge(_d)[_slc]
 
         if inverse:
-            return keep - x
+            return keep - _x
         else:
-            return x
+            return _x

chirpy/create/moments.py
@@ -70,9 +70,11 @@ class OriginGauge(_CORE):
 
         return new
 
-    def switch_origin_gauge(self, origins, assignment, number_of_types=None):
+    def switch_origin_gauge(self, origins_aa, assignment,
+                            number_of_types=None):
         '''origins in angstrom'''
-        if (_n_o := len(origins)) > len(self.r_au):
+        origins_au = origins_aa * constants.l_aa2au
+        if (_n_o := len(origins_aa)) > len(self.r_au):
             raise ValueError('new number of origins cannot be greater than '
                              'the old one (assignment failure)')
         # --- decompose according to assignment
@@ -84,13 +86,13 @@ class OriginGauge(_CORE):
         # --- process gauge-dependent properties
         # --- --- magnetic dipole
         _md_p = [ed.switch_magnetic_origin_gauge(_c, _m, _r,
-                                                 _o*constants.l_aa2au,
+                                                 _o,
                                                  cell_au_deg=self.cell_au_deg)
-                 for _o, _r, _c, _m in zip(origins, _rd, _cd, _md)]
+                 for _o, _r, _c, _m in zip(origins_au, _rd, _cd, _md)]
         # --- --- electric dipole
         # --- --- multipole
 
         # --- sum contributions
-        self.r_au = origins
+        self.r_au = origins_au
         self.c_au = _np.array([_cd[_i].sum(axis=0) for _i in range(_n_o)])
         self.m_au = _np.array([_md_p[_i].sum(axis=0) for _i in range(_n_o)])