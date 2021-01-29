a12f29c4fc61acba9af8165716e099454345d737
Sascha Jähnigen <sascha.jahnigen@ens.fr>
2021-01-29 18:15:07 +0100

    added velocity variance tools

--- chirpy/physics/molsim_utils.py
+++ chirpy/physics/molsim_utils.py
@@ -1,5 +1,5 @@
 # -------------------------------------------------------------------
-# 
+#
 #  ChirPy
 #
 #    A buoyant python package for analysing supramolecular
@@ -30,3 +30,54 @@
 
 import numpy as np
 
+from chirpy.classes import trajectory
+from chirpy.physics import constants
+from chirpy.physics.statistical_mechanics import spectral_density
+
+from chirpy.classes import _PALARRAY
+
+# this is essentially ft_colvar_cor (but without the sum)
+# to avoid memory issues, localisation etc has to happen here as well?
+# (don't keep ft_colvar_cor)
+
+
+def _func(x0, x1):
+    _omega, _S, _R = spectral_density(x0, x1, ts=1, flt_pow=-1, mode='A')
+    return _S.sum(axis=-1)
+
+
+def vv_variance(vel):
+    _n_frames, _n_atoms, _dim = vel.shape
+    vel = vel.reshape((_n_frames, _n_atoms * _dim))
+
+    JOB = _PALARRAY(_func, vel.T, repeat=2, upper_triangle=True, n_cores=6)
+    S = JOB.run()
+
+    # --- see comment in spectral_density module for explanation of prefactors
+    return S * 2 * np.pi / _n_frames
+
+
+_dir = '/home/jaehnigen/trajectories_nobackup/l-alanine-crystal/P212121_2x1x2'\
+       '/production/'
+_load = trajectory.XYZ(_dir + 'traj.xyz', range=(0, 64, 100000))
+
+TRAJ = _load.expand()
+
+print(TRAJ.data.shape)
+_n_frames, _n_atoms, _dim = TRAJ.data.shape
+
+# ts = 4
+POW = vv_variance(TRAJ.vel_au, n_cores=4)
+
+masses_au = constants.symbols_to_masses(sorted(3 * TRAJ.symbols)) \
+        * constants.m_amu_au
+
+effective_mass = constants.k_B_au * 337 / np.diag(POW) / masses_au
+
+np.set_printoptions(precision=2, linewidth=100)
+
+print('EFFECTIVE MASS')
+print('1.00000 = %5.5f ?' % np.mean(effective_mass))
+# print(effective_mass)
+
+# NEXT: localisation
