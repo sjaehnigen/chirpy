bugfix 0.22.x: corrected gauge_transport lever for pseudo_isolated VCD computation

chirpy/physics/spectroscopy.py
@@ -341,15 +341,10 @@ def _spectrum_from_tcf(*args,
             #                f'origin(s) {origin_au}',
             #                _ChirPyWarning,
             #                stacklevel=2)
-
-            pos = origin_au[:, None] + distance_pbc(origin_au[:, None],
-                                                    positions_au,
-                                                    cell_au_deg)
-            # --- old code, "sit" on the origin (!= common for all times)
-            # pos = distance_pbc(origin_au[:, None],
-            #                    positions_au,
-            #                    cell_au_deg)
-            # origin_au = np.zeros_like(origin_au)
+            pos = distance_pbc(origin_au[:, None],
+                               positions_au,
+                               cell_au_deg)
+            origin_au = np.zeros_like(origin_au)
             cell = None
         else:
             pos = copy.deepcopy(positions_au)